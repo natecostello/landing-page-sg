4957bdf9872e423a1a1a547e3a2c34794a704102
Nate Costello <natecostello@gmail.com>
2021-11-07 01:38:54 +0000

    added theme files to allow shadowing, and content

---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        
+        {` `}
+        <Link
+          aria-label="Link to the sites's GitHub repository"
+          href="https://github.com/natecostello/landing-page"
+        >
+          Github repo for this site,
+        </Link>
+        <div sx={{ mx: 1 }}>a lightly modified </div>
+        {` `}
+        <Link
+          aria-label="Link to the theme's Github repository"
+          href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara"
+        >
+          Cara Theme by LekoArts
+        </Link>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
