47a3723a71d9e67ea23f83dd97c0af119b8e4667
Nate Costello <natecostello@gmail.com>
2021-11-07 02:08:36 +0000

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `TF.S.I`,
+    siteTitleAlt: `tunafish.sandwich.industries`,
+    siteHeadline: `tunafish.sandwich.industries`,
+    siteUrl: `https://tunafish.sandwich.industries`,
+    siteDescription: `A personal landing page`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    // siteImage: `/banner.jpg`,
+    author: `natecostello`,
   },
   plugins: [
     {
@@ -58,4 +58,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
